7b6984f6d0804fc1c2d06ce1e9852d800d0fa017
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-03-14 20:38:00 -0400

    ADD: MEDIA QUERY: WAYS TO HELP, AND BLOG & NEWS

--- routes/team.js
+++ routes/team.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/team/:member', function (req, res) {
+router.get('/team/:member', function(req, res) {
   console.log(req.params.member);
-  res.render('team', { person: team[req.params.member] });
-});
+
+  res.render('team', { person: team[req.params.member]});
+})
 
 module.exports = router;
