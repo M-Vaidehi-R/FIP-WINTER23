32c1738bd7a43fb50ef4e8eb5fdbc09327951562
Mamatha Vaidehi Ramesh <m_ramesh150220@fanshaweonline.ca>
2023-04-07 11:48:07 -0400

    Create for volunteers working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var getsupportRouter = require('./routes/getsupport');
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/getsupport', getsupportRouter);
@@ -44,5 +46,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
